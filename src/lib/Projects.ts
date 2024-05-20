3eba95b8f96e90603d55d023b9d0f4e367e3426b
Rajeev K Rao <rajeevraok@gmail.com>
2024-05-20 18:14:18 +0530

    Adding Files

---
+++ src/lib/Projects.ts
@@ -0,0 +1,30 @@
+const projects = [
+	{
+		title: 'Branchout',
+		technologies: ['Node', 'HTML', 'CSS'],
+		description:
+			'A Landing Page for a co-working space company called Branchout.',
+		url: 'https://branchout.vercel.app/'
+	},
+	{
+		title: 'SmashFit',
+		technologies: ['NodeJs', 'React', 'MongoDb', 'GoogleAPIs'],
+		description:
+			'A fitness website that allows company to manage user registrations.',
+		url: 'https://smashfit.rkrao.me/'
+	},
+	{
+		title: 'Carrybot - Discord Bot',
+		technologies: ['Discord.js', 'React', 'MongoDB', 'Redis'],
+		description: 'A simple discord bot dedicated to Carryminati.',
+		url: 'https://github.com/rajeevkrao/carrybot'
+	},
+	{
+		title: 'Valorant Ranks - Github Pages',
+		technologies: ['Github Pages', 'JSON'],
+		description: 'A dedicated github page for serving valorant ranks and rank icons.',
+		url: 'https://github.com/rajeevkrao/valorant-ranks'
+	}
+];
+
+export default projects;
---
+++ src/lib/Skills.ts
@@ -0,0 +1,10 @@
+const skills = {
+	programming: ['Node.js', 'TypeScript', 'React.js', 'Svelte.js', 'Wails', 'Angular'],
+	// devops: [],
+	databases: ['MongoDB', 'Redis', 'CosmosDB'],
+	// tools: [],
+	languages: ['English', 'Kannada', 'Hindi'],
+	other: ['Design principles', 'User interface design']
+};
+
+export default skills;
